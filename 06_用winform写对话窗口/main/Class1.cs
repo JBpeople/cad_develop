f21e6b2c900b5471b8a1601ce43fb60f5264a683
JBpeople <lcmmljs@foxmail.com>
2022-03-20 11:09:11 +0800

    2022-03-20

---
+++ 06_用winform写对话窗口/main/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Drawing;
+using Autodesk.AutoCAD.DatabaseServices;// (Database, DBPoint, Line, Spline)
+using Autodesk.AutoCAD.Geometry;//(Point3d, Line3d, Curve3d)
+using Autodesk.AutoCAD.ApplicationServices;// (Application, Document)
+using Autodesk.AutoCAD.Runtime;// (CommandMethodAttribute, RXObject, CommandFlag)
+using Autodesk.AutoCAD.EditorInput;//(Editor, PromptXOptions, PromptXResult)
+using AcadApp = Autodesk.AutoCAD.ApplicationServices.Application;
+using Autodesk.AutoCAD.Colors;
+using static main.PtCadLib;
+
+namespace main
+{
+    public class Class1
+    {
+        [CommandMethod("aa")]
+        public static void JudgmentSlash()
+        {
+            PtCadLib ac = new PtCadLib();
+
+            FirstForm frm = new FirstForm();
+            //Autodesk.AutoCAD.ApplicationServices.Application.ShowModalDialog(frm); //在CAD中模态显示窗口
+            Autodesk.AutoCAD.ApplicationServices.Application.ShowModelessDialog(frm); //在CAD中非模态显示窗口
+        }
+    }
+}
+
---
+++ 06_用winform写对话窗口/main/FirstForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static main.PtCadLib;
+using System.Runtime.InteropServices; //应用[DllImport("user32.dll")]需要
+
+namespace main
+{
+    public partial class FirstForm : Form
+    {
+        [DllImport("user32.dll", EntryPoint = "SetFocus")]
+        public static extern int SetFocus(IntPtr hWnd);
+        public FirstForm()
+        {
+            InitializeComponent(); //初始化窗口
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PtCadLib ac = new PtCadLib();
+
+            SetFocus(ac.AcDoc.Window.Handle); //切换窗口焦点 非模态显示窗口时需要做该设置
+
+            ac.WriteMessage(textBox1.Text + "\n");
+        }
+    }
+}
